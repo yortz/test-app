655280ab4e1152bebb40a446cd4daaaab705ad3a
Davide D'Agostino <DAddYE@MacBook.local>
2009-11-16 18:12:10 +0100

    Namespaced generators.

--- lib/padrino/padrino-gen/generators/components/scripts/rightjs_script_gen.rb
+++
@@ -1,9 +0,0 @@
-module SinatraMore
-  module RightjsScriptGen
-
-    def setup_script
-      get("http://rightjs.org/builds/current/right-min.js", "public/javascripts/right-min.js")
-    end
-    
-  end
-end
