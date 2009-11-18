046902534e096ecf23f5ff8b15dd8ac4a435160e
Nathan Esquenazi <nesquena@gmail.com>
2009-11-18 01:01:24 -0800

    Using new mount_core function to clean up single application

--- config/apps.rb
+++ config/apps.rb
@@ -1,2 +1,2 @@
 # This file mount each application to a specific path.
-Padrino.mount("root", :app_class => "Website", :app_file => Padrino.root('app.rb')).to("/")
+Padrino.mount_core(:app_class => "Website")
