1a76a68b57bc0d9a20be84166ddec29e87a03718
Nathan Esquenazi <nesquena@gmail.com>
2009-11-24 02:38:47 -0800

    Generated the posts controller and added .components file back into repo

---
+++ app/controllers/posts.rb
@@ -0,0 +1,17 @@
+# Define your named routes inside config/urls.rb or define them inline
+# You can also use unnamed routes by defining them directly
+
+Blog.controllers do
+  # get :index do
+  #   session[:foo] = "bar"
+  #   haml_template 'index'
+  # end
+  
+  # get :sample, :map => "/sample/url" do
+  #   "Hello world!"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+end
