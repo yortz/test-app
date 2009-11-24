2a34818b9f4a738449c6bb38400a583926bc6831
Davide D'Agostino <DAddYE@MacBook.local>
2009-11-24 18:26:08 +0100

    Now we use our super beautiful logger! ;)

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,6 @@
 module DatabaseSetup
   def self.registered(app)
+    app.configure { DataMapper.logger = logger }
     app.configure(:development) { DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_#{Padrino.env.to_s}.db")) }
     app.configure(:production)  { DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_#{Padrino.env.to_s}.db")) }
     app.configure(:test)        { DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_#{Padrino.env.to_s}.db")) }
